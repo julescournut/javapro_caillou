dc110c76dc784f48cdb0c30ab3623b5d0b33bcaf
Jules COURNUT <jules.cournut@hotmail.fr>
2019-02-19 19:28:06 +0100

    initial commit

---
+++ src/main/java/isima/caillou/CaillouApplication.java
@@ -0,0 +1,13 @@
+package isima.caillou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaillouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaillouApplication.class, args);
+	}
+
+}
